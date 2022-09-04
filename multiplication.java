c7e128a086b10b91ddadca5da13d21d2f9d793c2
Tathagata558 <110629061+ghoshtathagata558@users.noreply.github.com>
2022-09-04 18:54:09 +0530

    Create multiplication.java

---
+++ multiplication.java
@@ -0,0 +1,14 @@
+import java.util.*;
+class multi
+{
+    public static void main()
+    {
+        Scanner Sc= new Scanner(System.in);
+        int n= Sc.nextInt();
+        {
+        for(int i=0;i<=12;++i)
+        System.out.println(n+" * "+i+" = "+n*i);  
+    }
+    
+}
+}
